d48125bbf23585db1f4a0eadf820abdd53d034df
All1n524 <alenhung0524@gmail.com>
2021-08-20 22:52:48 +0800

    my file

---
+++ BasicAndPrinting/LetterToYourself/LetterToYourself.java
@@ -0,0 +1,12 @@
+public class LetterToYourself
+{
+  public static void main( String[]args )
+  {
+    System.out.println("       #####");
+    System.out.println("       #####");
+    System.out.println("  ");
+    System.out.println("Allen");
+    System.out.println("PacificAmericanSchool");
+    System.out.println("Fuxing Road");
+  }
+}
