1a096bfee3b761a84b0ae9c1fc1a5e7ecf804d2d
All1n524 <alenhung0524@gmail.com>
2021-08-23 13:18:46 +0800

    my file

---
+++ BasicAndPrinting/YourInitials/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "      A        H       H" ); // and the comment after is ignored.
+        System.out.println( "     A A       H       H " );
+        System.out.println( "    A   A      HHHHHHHHH ");
+        System.out.println( "   AAAAAAA     H       H" );
+        System.out.println( "  A       A    H       H " );
+        System.out.println( " A         A   H       H " );
+    }
+}
