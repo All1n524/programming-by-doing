279f2066c4ddaf4df12134a274af1cd6e17d3c67
All1n524 <alenhung0524@gmail.com>
2021-08-24 14:37:24 +0800

    my file

---
+++ Variables/UsingVariables/UsingVariables.java
@@ -0,0 +1,15 @@
+public class UsingVariables
+{
+  public static void main( String[] args )
+  {
+  int room;
+  double e;
+  String className;
+  room = 113;
+  e = 2.71828;
+  className = "Computer science";
+  System.out.println("This is room #" + room);
+  System.out.println("e is close to" + e);
+  System.out.println("I'm learning a bit about " + className);
+  }
+}
