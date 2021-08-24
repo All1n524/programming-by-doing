e72b8bfde41eb69bff17b242b7fd99020ac3503f
All1n524 <alenhung0524@gmail.com>
2021-08-24 22:51:53 +0800

    my file

--- Variables/VariablesAndNames/VariablesAndNames.java
+++ Variables/VariablesAndNames/VariablesAndNames.java
@@ -2,16 +2,27 @@ public class VariablesAndNames
 {
     public static void main( String[] args )
     {
+        //using 4.0 for space_in_a_car it's necessary because it's more accurate. If it's just 4, then the computer will just simply round to the nearest non-decial degit.
+        //floating numbers are numbers with decimals.
+        // the below variables are all integers
         int cars, drivers, passengers, cars_not_driven, cars_driven;
+        // the below variables are all numbers with or without decimals
         double space_in_a_car, carpool_capacity, average_passengers_per_car;
-
+        //variable cars
         cars = 100;
+        //the space in a car
         space_in_a_car = 4.0;
+        //the drivers
         drivers = 30;
+        //the passengers
         passengers = 90;
+        // the cars not driven
         cars_not_driven = cars - drivers;
+        // the cars driven
         cars_driven = drivers;
+        // carpool capacity
         carpool_capacity = cars_driven * space_in_a_car;
+        // the average passengers per car
         average_passengers_per_car = passengers / cars_driven;
 
 
